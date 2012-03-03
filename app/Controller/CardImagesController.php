767b16aa5f8269075bfdc7495fb839ab6f51f3bc
root <root@dax-VPCEA43EG.(none)>
2012-03-03 17:11:55 +0800

    Some image upload tests. Merging CardImagesController into ImagesController.

--- app/Controller/CardImagesController.php
+++ app/Controller/CardImagesController.php
@@ -6,76 +6,74 @@ class CardImagesController extends AppController {
     
     public $components = array('ImageResizer');
     
-    public $uses = array('CardImage', 'Image');
+    public $uses = array('CardImage', 'Image');    
     
     
-    public function add() {
-        if($this->request->is('post')) {
-            $this->upload_images('1909_T206_TyCobb_WhiteSleaves_OldMill', 'card_variations', 1);
-        }
-        
-    }
-    
-    public function upload_images($card_image_name, $group_image_type, $group_id) {    
+    public function upload_images($params) {    
         $this->layout = false;
         $this->autoRender = false;
         
         $card_images_dir = Configure::read('card_images_dir');
-        
-        if($this->request->is('post')) {      
-            
-            $card_frontside = $this->request->data['CardImage']['card_front_side'];
-            $card_backside = $this->request->data['CardImage']['card_back_side'];
-            $card_orientation = $this->request->data['CardImage']['card_orientation'];            
+        $card_image_name = $params['filename'];
+        $image_group_type = $params['image_group_type'];
+        $image_group_id = $params['image_group_id'];                           
+        $card_frontside = $params['front_img'];
+        $card_backside = $params['rear_img'];
+        $card_orientation = $params['card_orientation'];            
 
-            $this->CardImage->set($this->request->data);
+        $validate_data = array('CardImage' => array('card_front_side' => $card_frontside,
+                                                    'card_back_side' => $card_backside));
+        $this->CardImage->set($validate_data);
 
-            if($this->CardImage->validates()) {
-                $card_sizes = Configure::read('card_image_sizes');                
+        if($this->CardImage->validates()) {
+            $card_sizes = Configure::read('card_image_sizes');                
 
-                $card_images = array('front' => $card_frontside, 'rear' => $card_backside);
-                $front_img = $rear_img = '';
-                
-                foreach($card_images as $side => $card_image) {
-                    //Rename file
-                    $filename_parts = explode('.', $card_image['name']);
-                    $file_extension = array_pop($filename_parts);
-                    $new_filename = $card_image_name .'_'. $side .'.'. $file_extension;                                                                      
+            $card_images = array('front' => $card_frontside, 'rear' => $card_backside);
+            $front_img = $rear_img = '';
 
-                    foreach($card_sizes as $size_label => $dimensions) {
-                        $upload_path = $card_images_dir . $group_image_type . DS . $group_id . $size_label;
-                        
-                        if(!is_dir($upload_path)) {
-                            @mkdir($upload_path, 0777, true);
-                        }                        
-                        
-                        $new_file = $upload_path . DS . $new_filename;
+            foreach($card_images as $side => $card_image) {
+                //Rename file
+                $filename_parts = explode('.', $card_image['name']);
+                $file_extension = array_pop($filename_parts);
+                $new_filename = $card_image_name .'_'. $side .'.'. $file_extension;                                                                      
 
-                        $prefs = array('resize_data' => array(RESIZE_WIDTH, $dimensions[$card_orientation], null));
-                        $errors = $this->ImageResizer->uploadFile($card_image['tmp_name'], $new_file, $prefs);
-                        
-                        if (!empty($errors)) {
-                            foreach ($errors as $key => $value) {
-                                $this->Session->setFlash('The following image resize error occured.<br />Error: ' . $value);
-                                
-                                return false;
-                            }
-                        } 
-                    }
-                    
-                    $data = array('Image' => array('file_name' => $new_filename));
-                    
-                    $this->Image->create();
-                    $this->Image->save($data);
-                }                                          
-            } else {
-                print_r($this->CardImage->validationErrors);
-                
-                return false;
-            } 
-            
+                foreach($card_sizes as $size_label => $dimensions) {
+                    $upload_path = $card_images_dir . $image_group_type . DS . $image_group_id . DS . $size_label;
+
+                    if(!is_dir($upload_path)) {
+                        @mkdir($upload_path, 0777, true);
+                    }                        
+
+                    $new_file = $upload_path . DS . $new_filename;
+
+                    $prefs = array('resize_data' => array(RESIZE_WIDTH, $dimensions[$card_orientation], null));
+                    $errors = $this->ImageResizer->uploadFile($card_image['tmp_name'], $new_file, $prefs);
+
+                    if (!empty($errors)) {
+                        $error_string = '';
+
+                        foreach ($errors as $key => $value) {
+                            $error_string .= 'Error: ' . $value . '<br />';                                                                                               
+                        }
+
+                        $this->Session->setFlash('The following image resize error occured.<br />'. $error_string);
+
+                        return false;
+                    } 
+                }
+
+                $data = array('Image' => array('file_name' => $new_filename));
+
+                $this->Image->create();
+                $this->Image->save($data);
+            }
+
             return true;
-        }                        
+        } else {                  
+            
+            //return $this->CardImage->validationErrors;
+            return false;
+        }                
     }
 
     public function popup($front_img_id = null, $rear_img_id) {        
@@ -85,50 +83,7 @@ class CardImagesController extends AppController {
 
         $this->set('card_image', $card_image);
         
-    }
-    
-    public function show_preview() {
-        $this->layout = false;
-        $this->autoRender = false;
-        
-        $card_images_dir = Configure::read('card_images_dir');
-        $card_sizes = Configure::read('card_image_sizes');
-        
-        if($this->request->data['CardImage']['preview_selected'] == 'front') {
-            $card_image = $this->request->data['CardImage']['card_front_side'];        
-        } else {
-            $card_image = $this->request->data['CardImage']['card_back_side'];        
-        }
-        
-        $card_orientation = $this->request->data['CardImage']['card_orientation'];
-        
-        $this->CardImage->set($this->request->data);
-        
-        if($this->CardImage->validates(array('fieldList' =>array('card_front_side')))) {
-            $upload_path = $card_images_dir . 'temp';
-
-            if(!is_dir($upload_path)) {
-                @mkdir($upload_path, 0777, true);
-            }                        
-
-            $filename_parts = explode('.', $card_image['name']);
-            $file_extension = array_pop($filename_parts);
-            $new_filename = time() .'.'. $file_extension;  
-
-            $new_file = $upload_path . DS . $new_filename;
-
-            $prefs = array('resize_data' => array(RESIZE_WIDTH, $card_sizes['thumbnail'][$card_orientation], null));
-            $errors = $this->ImageResizer->uploadFile($card_image['tmp_name'], $new_file, $prefs);
-
-            if(empty($errors)) {
-                echo '<img src="/files/card_images/temp/' . $new_filename . '" />';
-            } else {
-                echo 'Error in previewing card image.';
-            }
-        } else {
-            print_r($this->CardImage->validationErrors);
-        }
-    }
+    }       
     
     public function delete($id = null) {
         if(is_numeric($id)) {
